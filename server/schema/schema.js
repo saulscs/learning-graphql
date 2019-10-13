3c97c04f4268ef7d65d87e568e2de8f7471d6a37
saulscs <saulcoronasantos@ymail.com>
2019-10-12 23:05:36 -0500

    first configuration and schema

---
+++ server/schema/schema.js
@@ -0,0 +1,23 @@
+const graphql = require('graphql');
+
+const {GraphQLObjectType, GraphQLString} = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString}
+    })
+});
+
+const RooQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type:BookType,
+            args:{id: {type: GraphQLString  } },
+            resolve (parent,args)
+        }
+    }
+});

328f236b36b21f5872ed0fed2ef074acd98d77eb
saulscs <saulcoronasantos@ymail.com>
2019-10-14 22:55:05 -0500

    Rendering Data in a Component

--- client/src/components/bookList.jsx
+++ client/src/components/bookList.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`;
 
-function BookList() {
-  return (
-    <div>
-      <ul id="book-list">
-          <li>Book Name</li>
-      </ul>
-    </div>
-  );
+class BookList extends Component {
+    displayBooks(){
+        let data = this.props.data
+        if (data.loading){
+            return (<div>Loading Books</div>)
+        } else {
+            return data.books.map(book => {
+                return (<li key={book.id}>{book.name}</li>)
+            })
+        }
+    }
+    render(){
+        console.log(this.props);
+        return(
+            <div>
+                <ul id="book-list">
+                    {this.displayBooks()}
+                </ul>
+            </div>
+        );
+    }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);

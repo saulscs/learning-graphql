aa812487ec7b573a0ded45f68ae6ee769ac26da7
saulscs <saulcoronasantos@ymail.com>
2019-10-14 21:03:26 -0500

    Apollo Client Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
 //components
 import BookList from './components/bookList';
 
+//Apollo Client setup
+
+const client = new ApolloClient({
+  uri: 'htpp://localhost:4000/graphql'
+})
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div id ="main">
       <h1>Reading List</h1>
       <BookList/>
     </div>
+    </ApolloProvider>
   );
 }
 

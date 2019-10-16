c58a5a5029c117d52f028dd1e9abd121ef37d61d
saulscs <saulcoronasantos@ymail.com>
2019-10-15 21:23:09 -0500

    Making a Single Query

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -4,11 +4,34 @@ import {getBookQuery} from '../querys/querys';
 
 
 class BookDetails extends Component {
+    displayBookDetails(){
+        const {book} =this.props.data;
+        if(book){
+            return(
+                <div>
+                    <h2>{book.name}</h2>
+                    <p>{book.genre}</p>
+                    <p>{book.author.name}</p>
+                    <p>All books by this author</p>
+                    <ul className="other-books">
+                        {
+                            book.author.books.map(item => {
+                                return <li key={item.id}>{item.name}</li>
+                            })
+                        }
+                    </ul>
+                </div>
+            )
+        } else{
+            return (
+                <div>No book selected ...</div>
+            )
+        }
+    }
     render(){ 
-        console.log(this.props);
         return(
             <div id ="book-details">
-                <p>Output book details here </p>
+               {this.displayBookDetails()}
             </div>
             
         );
